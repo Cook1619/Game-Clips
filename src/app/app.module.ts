3d32dcc06d728b68cbfaed692716b1cadca3d60e
Matt Cook <mathew.cook@mastery.net>
2023-02-01 15:30:25 -0600

    Creating component to handle images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   // Angular doesn't assume what your building, this is a browser application, so we use Browser Module
   imports: [
